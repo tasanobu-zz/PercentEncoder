fbd5f0cc5e8b351168432c9b083f56a9292c9329
tasaka-kazunobu <tasaka_kazunobu@cyberagent.co.jp>
2015-08-04 15:19:43 +0900

    Add PercentEncoding enum

---
+++ PercentEncoder/PercentEncoder/PercentEncoding.swift
@@ -0,0 +1,34 @@
+//
+//  PercentEncoding.swift
+//  PercentEncoder
+//
+//  Created by Kazunobu Tasaka on 8/4/15.
+//  Copyright (c) 2015 Kazunobu Tasaka. All rights reserved.
+//
+
+import Foundation
+import JavaScriptCore
+
+public enum PercentEncoding {
+    case EncodeURI, EncodeURIComponent, DecodeURI, DecodeURIComponent
+    
+    /// return equivalent javascript function name
+    private var functionName: String {
+        switch self {
+        case .EncodeURI:            return "encodeURI"
+        case .EncodeURIComponent:   return "encodeURIComponent"
+        case .DecodeURI:            return "decodeURI"
+        case .DecodeURIComponent:   return "decodeURIComponent"
+        }
+    }
+    
+    public func evaluate(#string: String) -> String {
+        // escape single quote becasue it is used in script string
+        let escaped = string.stringByReplacingOccurrencesOfString("'", withString: "\\'")
+        let script = "var value = \(functionName)('\(escaped)');"
+        let context = JSContext()
+        context.evaluateScript(script)
+        let value: JSValue = context.objectForKeyedSubscript("value")
+        return value.toString()
+    }
+}
