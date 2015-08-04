1e1ecfd83d46aac2861f15346f90a922f8036dda
tasaka-kazunobu <tasaka_kazunobu@cyberagent.co.jp>
2015-08-04 15:18:47 +0900

    Add the project

---
+++ PercentEncoder/PercentEncoder/PercentEncoder.h
@@ -0,0 +1,19 @@
+//
+//  PercentEncoder.h
+//  PercentEncoder
+//
+//  Created by Kazunobu Tasaka on 8/4/15.
+//  Copyright (c) 2015 Kazunobu Tasaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PercentEncoder.
+FOUNDATION_EXPORT double PercentEncoderVersionNumber;
+
+//! Project version string for PercentEncoder.
+FOUNDATION_EXPORT const unsigned char PercentEncoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PercentEncoder/PublicHeader.h>
+
+
---
+++ PercentEncoder/PercentEncoderTests/PercentEncodingTests.swift
@@ -0,0 +1,35 @@
+//
+//  PercentEncoderTests.swift
+//  PercentEncoderTests
+//
+//  Created by Kazunobu Tasaka on 8/4/15.
+//  Copyright (c) 2015 Kazunobu Tasaka. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import PercentEncoder
+
+class PercentEncodingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
