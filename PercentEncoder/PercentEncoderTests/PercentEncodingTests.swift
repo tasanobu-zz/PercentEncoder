8cfb22eff32a1eb23c310c0de3e3b7168efc6b65
tasaka-kazunobu <tasaka_kazunobu@cyberagent.co.jp>
2015-08-04 15:56:23 +0900

    Add tests for PercentEncoding

--- PercentEncoder/PercentEncoderTests/PercentEncodingTests.swift
+++ PercentEncoder/PercentEncoderTests/PercentEncodingTests.swift
@@ -11,6 +11,9 @@ import XCTest
 import PercentEncoder
 
 class PercentEncodingTests: XCTestCase {
+    let input = "http://tasanobu.jp?city=東京&year='20"
+    let inputEscapedByEncodeURI = "http://tasanobu.jp?city=%E6%9D%B1%E4%BA%AC&year='20"
+    let inputEscapedByEncodeURIComponent = "http%3A%2F%2Ftasanobu.jp%3Fcity%3D%E6%9D%B1%E4%BA%AC%26year%3D'20"
     
     override func setUp() {
         super.setUp()
@@ -19,10 +22,32 @@ class PercentEncodingTests: XCTestCase {
     override func tearDown() {
         super.tearDown()
     }
+
+    // MARK: EncodeURI
+    func testEncodeURI() {
+        let encoded = PercentEncoding.EncodeURI.evaluate(string: input)
+        XCTAssertEqual(
+            encoded,
+            "http://tasanobu.jp?city=%E6%9D%B1%E4%BA%AC&year='20",
+            "")
+    }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testEncodeURIComponent() {
+        let encoded = PercentEncoding.EncodeURIComponent.evaluate(string: input)
+        XCTAssertEqual(
+            encoded,
+            "http%3A%2F%2Ftasanobu.jp%3Fcity%3D%E6%9D%B1%E4%BA%AC%26year%3D'20",
+            "")
+    }
+    
+    func testDecodeURI() {
+        let decoded = PercentEncoding.DecodeURI.evaluate(string: inputEscapedByEncodeURI)
+        XCTAssertEqual(decoded, input, "")
+    }
+    
+    func testDecodeURIComponent() {
+        let decoded = PercentEncoding.DecodeURIComponent.evaluate(string: inputEscapedByEncodeURIComponent)
+        XCTAssertEqual(decoded, input, "")
     }
     
     func testPerformanceExample() {
